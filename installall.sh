0e00f3b0def569448e70225cdc055a534a7a9010
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-06-13 23:42:52 +0200

    Fix Again Critical Bugs

--- installall.sh
+++ installall.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Uncursus Install Script V0.5.2.2 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
+echo -e "\e[31mWelcome to Uncursus Install Script V0.5.2.4 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
 echo "WARNING : THIS IS IN ALPHA A9-A11 IS ONLY SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
 echo "If you found bug pls create an issues in github ;)"
 echo "Enjoy :)"
@@ -26,18 +26,18 @@ dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
 mkdir /User/Documents/Uncursus/u0
 wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
-unzip /User/Documents/Uncursus/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
+unzip /User/Documents/Uncursus/DebPatch.zip?raw\=true
 rm -rf /usr/bin/cyinject
 wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 echo "Done. Installing necessary debs for patch."
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/coreutils.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/xz-utils.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/coreutils-bin_8.31-1_all.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/libssl.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/lzma.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/ncurses5-libs_5.9-1_all.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/xz_5.2.4-4_all.deb
+dpkg -i --force-all /User/Documents/Uncursus/coreutils.deb
+dpkg -i --force-all /User/Documents/Uncursus/xz-utils.deb
+dpkg -i --force-all /User/Documents/Uncursus/coreutils-bin_8.31-1_all.deb
+dpkg -i --force-all /User/Documents/Uncursus/libssl.deb
+dpkg -i --force-all /User/Documents/Uncursus/lzma.deb
+dpkg -i --force-all /User/Documents/Uncursus/ncurses5-libs_5.9-1_all.deb
+dpkg -i --force-all /User/Documents/Uncursus/xz_5.2.4-4_all.deb
 dpkg -i --force-all /User/Documents/Uncursus/u0/com.ex.substitute_0.1.14_iphoneos-arm.deb.deb com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb
 echo "Done. Running Firmware Configuration (./firmware.sh)"
 /usr/libexec/firmware
