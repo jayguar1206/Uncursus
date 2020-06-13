0883a2525b7bc45d9f8d4cbebbc4efef3cd78595
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-06-14 00:30:20 +0200

    First Beta Release All Bugs Fixed Should Good Now

--- installall.sh
+++ installall.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Uncursus Install Script V0.5.2.3 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
+echo -e "\e[31mWelcome to Uncursus Install Script V1.0.2 (Beta) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
 echo "WARNING : THIS IS IN ALPHA A9-A11 IS ONLY SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
 echo "If you found bug pls create an issues in github ;)"
 echo "Enjoy :)"
@@ -27,7 +27,7 @@ echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/
 mkdir /User/Documents/Uncursus/u0
 wget https://github.com/Yaya48/Uncursus/blob/new/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
 unzip /User/Documents/Uncursus/DebPatch.zip?raw\=true -d /User/Documents/Uncursus/DebPatch
-rm -rf /usr/bin/cyinject
+rm -rf /usr/bin/cynject
 wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 echo "Done. Installing necessary debs for patch."
@@ -38,12 +38,12 @@ dpkg -i --force-all /User/Documents/Uncursus/DebPatch/libssl.deb
 dpkg -i --force-all /User/Documents/Uncursus/DebPatch/lzma.deb
 dpkg -i --force-all /User/Documents/Uncursus/DebPatch/ncurses5-libs_5.9-1_all.deb
 dpkg -i --force-all /User/Documents/Uncursus/DebPatch/xz_5.2.4-4_all.deb
-dpkg -i --force-all /User/Documents/Uncursus/u0/com.ex.substitute_0.1.14_iphoneos-arm.deb.deb com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb
+dpkg -i --force-all /User/Documents/Uncursus/u0/*.deb
 echo "Done. Running Firmware Configuration (./firmware.sh)"
 /usr/libexec/firmware
 echo "BootStrap Installions Done. The Installer Clean The Installions"
 #rm -rf /User/Documents/Uncursus/
-rm /etc/apt/source.list.d/odyssey.sources
+rm /etc/apt/sources.list.d/odyssey.sources
 echo "All Done."
 killall SpringBoard
 fi
