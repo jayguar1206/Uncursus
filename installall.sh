1ec1a3416ae46e23f1a327e371c6eac3263c1cc1
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-06-13 20:34:54 +0200

    Attempts To Fix The Critical Bugs

--- installall.sh
+++ installall.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Uncursus Install Script V0.3.1 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
+echo -e "\e[31mWelcome to Uncursus Install Script V0.4 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
 echo "WARNING : THIS IS IN ALPHA A9-A11 IS ONLY SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
 echo "If you found bug pls create an issues in github ;)"
 echo "Enjoy :)"
@@ -19,8 +19,8 @@ rm -rf /User/Documents/Uncurus
 mkdir /User/Documents/Uncurus
 apt update
 apt install wget -y --allow-unauthenticated
-wget https://github.com/Yaya48/Uncurus/blob/master/DebProcurusSystem.zip --directory-prefix=/User/Documents/Uncurus/
-unzip /User/Documents/Uncurus/DebProcurusSystem.zip -d /User/Documents/Uncurus/
+wget https://github.com/Yaya48/Uncurus/blob/master/DebProcurusSystem.zip?raw=true --directory-prefix=/User/Documents/Uncurus/
+unzip /User/Documents/Uncurus/DebProcurusSystem.zip?raw=true -d /User/Documents/Uncurus/
 dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
 mkdir /User/Documents/Uncurus/DebPatch
@@ -32,7 +32,7 @@ wget https://github.com/Yaya48/Uncurus/blob/master/DebPatch/libssl.deb --directo
 wget https://github.com/Yaya48/Uncurus/blob/master/DebPatch/lzma.deb --directory-prefix=/User/Documents/Uncurus/DebPatch
 wget https://github.com/Yaya48/Uncurus/blob/master/DebPatch/ncurses5-libs_5.9-1_all.deb --directory-prefix=/User/Documents/Uncurus/DebPatch
 wget https://github.com/Yaya48/Uncurus/blob/master/DebPatch/xz_5.2.4-4_all.deb --directory-prefix=/User/Documents/Uncurus/DebPatch
-wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.0.15_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncurus/u0
+wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncurus/u0
 wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncurus/u0
 echo "Done. Installing necessary debs for patch."
 dpkg -i --force-all /User/Documents/Uncurus/DebPatch/coreutils.deb
