74609617894481b1a158e4f8dbf1f3108c635d60
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-06-13 20:23:08 +0200

    Try To Fix A Critical Bug

--- installall.sh
+++ installall.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Uncursus Install Script V0.3 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
+echo -e "\e[31mWelcome to Uncursus Install Script V0.3.1 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
 echo "WARNING : THIS IS IN ALPHA A9-A11 IS NOT SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
 echo "If you found bug pls create an issues in github ;)"
 echo "Enjoy :)"
@@ -15,14 +15,14 @@ apt install curl -y
 echo "Downloading And Executing Offical Procurus Script From Coolstar"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Yaya48/Uncurus/master/procursus-deploy-u0.sh)"
 echo "Downloading And Installing Offical Procurus Deb"
+rm -rf /User/Documents/Uncurus
+mkdir /User/Documents/Uncurus
 apt update
 apt install wget -y --allow-unauthenticated
 wget https://github.com/Yaya48/Uncurus/blob/master/DebProcurusSystem.zip --directory-prefix=/User/Documents/Uncurus/
 unzip /User/Documents/Uncurus/DebProcurusSystem.zip -d /User/Documents/Uncurus/
-dpkg -i /User/Documents/DebProcurusSystem/*.deb
+dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
-rm -rf /User/Documents/Uncurus
-mkdir /User/Documents/Uncurus
 mkdir /User/Documents/Uncurus/DebPatch
 mkdir /User/Documents/Uncurus/u0
 wget https://github.com/Yaya48/Uncurus/blob/master/DebPatch/coreutils.deb --directory-prefix=/User/Documents/Uncurus/DebPatch
