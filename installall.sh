9090e2a7d1e671a6106f7d3d751b6661a5c9263b
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-06-13 23:47:40 +0200

    Fix Links

--- installall.sh
+++ installall.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Uncursus Install Script V0.5.2.4 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
+echo -e "\e[31mWelcome to Uncursus Install Script V0.5.2.4.1 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
 echo "WARNING : THIS IS IN ALPHA A9-A11 IS ONLY SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
 echo "If you found bug pls create an issues in github ;)"
 echo "Enjoy :)"
@@ -14,18 +14,18 @@ apt install com.bingner.plutil -y
 apt install zsh -y
 apt install curl -y
 echo "Downloading And Executing Offical Procurus Script From Coolstar"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Yaya48/Uncursus/master/procursus-deploy-u0.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Yaya48/Uncursus/new/procursus-deploy-u0.sh)"
 echo "Downloading And Installing Offical Procurus Deb"
 rm -rf /User/Documents/Uncursus
 mkdir /User/Documents/Uncursus
 apt update
 apt install wget -y --allow-unauthenticated
-wget https://github.com/Yaya48/Uncursus/blob/master/DebProcurusSystem.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
+wget https://github.com/Yaya48/Uncursus/blob/new/DebProcurusSystem.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
 unzip /User/Documents/Uncursus/DebProcurusSystem.zip?raw=true -d /User/Documents/Uncursus/
 dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
 mkdir /User/Documents/Uncursus/u0
-wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
+wget https://github.com/Yaya48/Uncursus/blob/new/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
 unzip /User/Documents/Uncursus/DebPatch.zip?raw\=true
 rm -rf /usr/bin/cyinject
 wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
