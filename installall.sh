651099bc391541282dc20000d374ac00a724eb8c
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-06-13 22:00:00 +0200

    Fix Critical Bug

--- installall.sh
+++ installall.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Uncursus Install Script V0.4.2 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
+echo -e "\e[31mWelcome to Uncursus Install Script V0.5 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
 echo "WARNING : THIS IS IN ALPHA A9-A11 IS ONLY SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
 echo "If you found bug pls create an issues in github ;)"
 echo "Enjoy :)"
@@ -24,15 +24,10 @@ wget https://github.com/Yaya48/Uncursus/blob/master/DebProcurusSystem.zip?raw=tr
 unzip /User/Documents/Uncursus/DebProcurusSystem.zip?raw=true -d /User/Documents/Uncursus/
 dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
-mkdir /User/Documents/Uncursus/DebPatch
 mkdir /User/Documents/Uncursus/u0
-wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch/coreutils.deb --directory-prefix=/User/Documents/Uncursus/DebPatch
-wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch/xz-utils.deb --directory-prefix=/User/Documents/Uncursus/DebPatch
-wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch/coreutils-bin_8.31-1_all.deb --directory-prefix=/User/Documents/Uncursus/DebPatch
-wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch/libssl.deb --directory-prefix=/User/Documents/Uncursus/DebPatch
-wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch/lzma.deb --directory-prefix=/User/Documents/Uncursus/DebPatch
-wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch/ncurses5-libs_5.9-1_all.deb --directory-prefix=/User/Documents/Uncursus/DebPatch
-wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch/xz_5.2.4-4_all.deb --directory-prefix=/User/Documents/Uncursus/DebPatch
+mkdir /User/Documents/Uncursus/DebPatch
+wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
+unzip /User/Documents/Procurus/DebPatch?raw=true --directory-prefix=/User/Documents/Uncursus/DebPatcha
 wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 echo "Done. Installing necessary debs for patch."
