f84c6ccf47b16c6ee86af471de7887e4c6729402
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-06-13 23:06:27 +0200

    Updated To Fix Critical Bugs

--- installall.sh
+++ installall.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Uncursus Install Script V0.5.2.1 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
+echo -e "\e[31mWelcome to Uncursus Install Script V0.5.2.2 (Alha) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
 echo "WARNING : THIS IS IN ALPHA A9-A11 IS ONLY SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
 echo "If you found bug pls create an issues in github ;)"
 echo "Enjoy :)"
@@ -25,9 +25,8 @@ unzip /User/Documents/Uncursus/DebProcurusSystem.zip?raw=true -d /User/Documents
 dpkg -i /User/Documents/Uncursus/DebProcurusSystem/*.deb
 echo "Done. Create Custom Directory For Download All Files Requied. Path (/User/Documents/)."
 mkdir /User/Documents/Uncursus/u0
-mkdir /User/Documents/Uncursus/DebPatch
 wget https://github.com/Yaya48/Uncursus/blob/master/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
-unzip /User/Documents/Uncursus/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/DebPatch
+unzip /User/Documents/Uncursus/DebPatch.zip?raw=true --directory-prefix=/User/Documents/Uncursus/
 rm -rf /usr/bin/cyinject
 wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
