553e1fa466edfed44761ad45448cc5076c976b4d
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-06-19 02:47:07 +0200

    Updated Things

--- installall.sh
+++ installall.sh
@@ -3,7 +3,7 @@ if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
 echo -e "\e[31mWelcome to Uncursus Install Script V1.0.2.1 (Beta) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
-echo "WARNING : THIS IS IN BETA A9-A11 IS ONLY SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
+echo "WARNING : THIS IS IN ALPHA A9-A11 IS ONLY SUPPORTED IM NOT RESPONSABLE IF ANYTHING GOES WRONG"
 echo "If you found bug pls create an issues in github ;)"
 echo "Enjoy :)"
 echo "Starting..."
@@ -31,8 +31,7 @@ rm -rf /usr/bin/cynject
 wget https://apt.bingner.com/debs/1443.00/com.ex.substitute_0.1.14_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 wget https://apt.bingner.com/debs/1443.00/com.saurik.substrate.safemode_0.9.6003_iphoneos-arm.deb --directory-prefix=/User/Documents/Uncursus/u0
 echo "Done. Installing necessary debs for patch."
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/coreutils.deb
-dpkg -i --force-all /User/Documents/Uncursus/DebPatch/xz-utils.deb
+dpkg -i --force-all /User/Documents/Uncursus/DebPatch/us.diatr.sileorespring_1.1_iphoneos-arm.deb
 dpkg -i --force-all /User/Documents/Uncursus/DebPatch/coreutils-bin_8.31-1_all.deb
 dpkg -i --force-all /User/Documents/Uncursus/DebPatch/libssl.deb
 dpkg -i --force-all /User/Documents/Uncursus/DebPatch/lzma.deb
